5a112c1252ce0027ceda8bbf70bbe7c8c775b2a6
Michał Jezierski <ambinanitelo@gmail.com>
2017-12-14 11:23:19 +0100

    parse tests + implementation

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 'use strict';
 
+// https://jsperf.com/test-protocol-indexof-vs-regex
+function getProtocol(url) {
+
+    if (url.indexOf('//') === 0) {
+        return '';
+    } else if (url.indexOf('https://') === 0) {
+        return 'https';
+    }
+
+    return 'http';
+}
+
+function getHostAndPath(url) {
+    const match = /(.*:?\/\/)?([^/^?]*)([^?]*)?/.exec(url);
+
+    if (match) {
+        const host = match[2];
+        const path = match[3] ? match[3].split('/').filter(p => p) : [];
+
+        return [ host, path ];
+    }
+}
+
 function parse(url) {
+    const [ host, path ] = getHostAndPath(url);
     const source = url.split('?');
-    const protocolHostPath = source[0].split(/\/\//);
-    const protocol = protocolHostPath[0].slice(0, -1) || '';
-    const hostPath = protocolHostPath[1].split(/\/(.+)/);
-    const host = hostPath[0] || '';
-    const path = hostPath[1] ? hostPath[1].split('/').filter(p => p.length) : [];
+
     const query = source[1] ? source[1].split('&').map(q => ({
         name: q.split('=')[0],
         value: q.split('=')[1]
     })) : [];
 
     return {
-        protocol,
+        protocol: getProtocol(url),
         host,
         path,
         query
