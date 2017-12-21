b53c24e71a57d70b51425a4cbd32a5fdb1f199d3
Michał Jezierski <ambinanitelo@gmail.com>
2017-12-21 18:45:04 +0100

    stringify performance improvements

--- tests/benchmark.js
+++ tests/benchmark.js
@@ -12,6 +12,8 @@ const url5 = 'http://domain.lol/foo/bar';
 const url6 = 'domain.lol/foo?foo=bar&baz=qux#hash';
 const url7 = 'https://some.very.long.domain1.com/some/really/long/path/lorem/ipsum/dolor/sit/amet?foo=bar&baz=qux&order=m&stan=nowe&dostawa-kurier=1&price_from=11&price_to=22&freeShipping=1&super-sprzedawca=1&city=Gda%C5%84sk&startingTime=3&a[]=1&a[]=2&a[]=3&a[]=4&a[]=5&a[]=6&a[]=7&a[]=8&b[]=1&b%3d[]=2&b[]=3&b%3d[]=4&b[]=5&b%3d[]=6&b[]=7&b%3d[]=8&route=https://domain.lol/foo/bar/baz/?advert=1&route=some-other-route#hash-bash-mome-long-and-ugly____--%3F--ha-s-h';
 
+console.log(JSON.stringify(ale.parse(url7)));
+
 suite
     .add('ale-url-parser/parse', () => {
         ale.parse(url1);
